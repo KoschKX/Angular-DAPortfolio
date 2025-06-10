ab0f2e4e14e1ef4c103a24e27566501135519a37
koschkx@gmail.com <96351407+KoschKX@users.noreply.github.com>
2025-06-10 11:41:58 +0200

    Added Privacy Policy page. Fixed CSS bug in Hero section.

--- src/app/subpage/subpage.ts
+++ src/app/subpage/subpage.ts
@@ -4,12 +4,13 @@ import { RouterOutlet, ActivatedRoute } from '@angular/router';
 
 import { NavbarComponent } from "../navbar/navbar.component";
 import { ImprintComponent } from "../imprint/imprint.component";
+import { PrivacyComponent } from "../privacy/privacy.component";
 import { FooterComponent } from "../footer/footer";
 
 @Component({
   selector: 'app-subpage',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, ImprintComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, ImprintComponent, PrivacyComponent, FooterComponent],
   templateUrl: './subpage.component.html',
   styleUrl: './supbage.component.scss'
 })
